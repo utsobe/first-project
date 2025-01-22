42bed30c072e28435161f38359f91acde79458cb
utsobe <mmhutsobe01@gmail.com>
2025-01-22 22:30:29 +0800

    [DONE] Create Student Feature

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,14 @@ import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
 
-main().catch((err) => console.log(err));
-
 async function main() {
   try {
+    console.log('Connecting to database...');
     await mongoose.connect(config.database_url as string);
+    console.log('Database connected');
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}!`);
+      console.log(`App listening on port ${config.port}!`);
     });
   } catch (error) {
     console.log(error);
